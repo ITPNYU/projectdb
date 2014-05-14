9d1b2522afd0b782a7e098ea1107880a9dca7bd7
Karl Ward <karl@ghostghost.net>
2014-05-13 22:15:50 -0400

    increased limit for results per page --KW

--- projectdb/__init__.py
+++ projectdb/__init__.py
@@ -14,12 +14,12 @@ login_manager.init_app(app)
 # Flask-Restless API endpoints
 # note: GET preprocessors pulled in via projectdb.authn.auth_func
 manager = APIManager(app, session=db_session, preprocessors=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func]))
-category_blueprint = manager.create_api(Category, methods=['GET'], collection_name='category', url_prefix='/v1')
-class_blueprint = manager.create_api(Class, methods=['GET'], url_prefix='/v1')
-project_blueprint = manager.create_api(Project, methods=['GET'], url_prefix='/v1')
-tag_blueprint = manager.create_api(Tag, methods=['GET'], collection_name='tag', url_prefix='/v1')
-term_blueprint = manager.create_api(Term, methods=['GET'], collection_name='term', url_prefix='/v1')
-venue_blueprint = manager.create_api(Venue, methods=['GET'], url_prefix='/v1')
+category_blueprint = manager.create_api(Category, methods=['GET'], collection_name='category', url_prefix='/v1', max_results_per_page=300)
+class_blueprint = manager.create_api(Class, methods=['GET'], url_prefix='/v1', max_results_per_page=300)
+project_blueprint = manager.create_api(Project, methods=['GET'], url_prefix='/v1', max_results_per_page=300)
+tag_blueprint = manager.create_api(Tag, methods=['GET'], collection_name='tag', url_prefix='/v1', max_results_per_page=300)
+term_blueprint = manager.create_api(Term, methods=['GET'], collection_name='term', url_prefix='/v1', max_results_per_page=300)
+venue_blueprint = manager.create_api(Venue, methods=['GET'], url_prefix='/v1', max_results_per_page=300)
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
